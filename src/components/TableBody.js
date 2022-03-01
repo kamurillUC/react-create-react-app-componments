3a27569c5d1bb033c7a2a41e0b9bcebfe484001d
kamurillUC <84762384+kamurillUC@users.noreply.github.com>
2022-02-28 21:50:33 -0600

    Commit

---
+++ src/components/TableBody.js
@@ -0,0 +1,20 @@
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Kevin</td>
+                <td>System Analyst</td>
+            </tr>
+             <tr>
+                <td>Lorenzo</td>
+                <td>System Analyst</td>
+            </tr>
+            <tr>
+                <td>David</td>
+                <td>Software Developer</td>
+            </tr>
+        </tbody>
+    );
+};
+
+export default TableBody;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import Table from './components/Table';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(<Table/>, document.getElementById('table-page'));
